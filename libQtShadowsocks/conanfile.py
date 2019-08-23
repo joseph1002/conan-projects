027fdc21af846c543194f577753040f155162b44
wallestar <joseph1002.lee@gmail.com>
2019-08-23 15:17:34 +0800

    initial commit

---
+++ libQtShadowsocks/conanfile.py
@@ -0,0 +1,33 @@
+from conans import ConanFile, CMake, tools
+
+
+class LibqtshadowsocksConan(ConanFile):
+    name = "libQtShadowsocks"
+    version = "v2.1.0"
+    license = "MIT"
+    url = "https://github.com/shadowsocks/libQtShadowsocks.git"
+    description = "libQtShadowsocks conan file"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+
+    def source(self):
+        self.run("git clone https://github.com/shadowsocks/libQtShadowsocks.git")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="libQtShadowsocks")
+        cmake.build()
+        cmake.install()
+
+    # def package(self):
+    #     self.copy("*.h", dst="include", src="libQtShadowsocks")
+    #     self.copy("*libQtShadowsocks.lib", dst="lib", keep_path=False)
+    #     self.copy("*.dll", dst="bin", keep_path=False)
+    #     self.copy("*.so", dst="lib", keep_path=False)
+    #     self.copy("*.dylib", dst="lib", keep_path=False)
+    #     self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["QtShadowsocks"]
---
+++ quazip/conanfile.py
@@ -0,0 +1,33 @@
+from conans import ConanFile, CMake, tools
+
+
+class Quazip5Conan(ConanFile):
+    name = "quazip"
+    version = "0.8.1"
+    license = "MIT"
+    url = "https://github.com/stachenov/quazip.git"
+    description = "quazip conan file"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+
+    def source(self):
+        self.run("git clone https://github.com/stachenov/quazip.git")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="quazip")
+        cmake.build()
+        cmake.install()
+
+    # def package(self):
+    #     self.copy("*.h", dst="include", src="libquazip5")
+    #     self.copy("*libquazip5.lib", dst="lib", keep_path=False)
+    #     self.copy("*.dll", dst="bin", keep_path=False)
+    #     self.copy("*.so", dst="lib", keep_path=False)
+    #     self.copy("*.dylib", dst="lib", keep_path=False)
+    #     self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["quazip5"]
